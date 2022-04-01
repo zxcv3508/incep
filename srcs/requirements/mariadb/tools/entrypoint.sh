5f975ed386013fed057c78ef5024c6c77fd2035b
Hyochan Park <hyopark@c3r3s7.42seoul.kr>
2022-04-01 20:09:13 +0900

    5

--- srcs/requirements/mariadb/tools/entrypoint.sh
+++ srcs/requirements/mariadb/tools/entrypoint.sh
@@ -1,3 +1,15 @@
-# service mysql start
+
+if [ ! -e /var/lib/mysql/$MYSQL_DB_NAME ]; then
+	chown -R mysql:mysql /var/lib/mysql
+	service mysql start
+	echo "sql start"
+	mysql -e  "CREATE DATABASE IF NOT EXISTS $MYSQL_DB_NAME;\
+	CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';\
+	GRANT ALL PRIVILEGES ON $MYSQL_DB_NAME.* TO '$MYSQL_USER'@'%' WITH GRANT OPTION;\
+	USE $MYSQL_DB_NAME;FLUSH PRIVILEGES;"
+	service mysql stop
+	echo "sql done"
+	mv /tmp/my.cnf /etc/mysql/my.cnf
+fi
+
 exec mysqld_safe
-# mysql
--- srcs/requirements/wp-php/tools/entrypoint.sh
+++
@@ -1,5 +0,0 @@
-service php7.3-fpm start
-service php7.3-fpm stop
-php-fpm7.3 -F
-
-#php-fpm7.3 --nodaemonize
